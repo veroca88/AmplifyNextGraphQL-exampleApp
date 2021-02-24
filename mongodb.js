859b9b8e0dfbb0890d486694485d6f81f526dbb8
veroca88 <veronicadonoso1506@gmail.com>
2021-02-23 21:31:14 -0600

    Replace mongoose with mongodb

--- mongodb.js
+++ mongodb.js
@@ -3,20 +3,25 @@ require('dotenv').config()
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
 
-const connectionURL = process.env.MONGODB_URL
-const databaseName = 'toDo-app'
+const connectionURL = process.env.MONGODB_URI
+const databaseName = "todo-app"
 
-MongoClient.connect(connectionURL, {
-    useNewUrlParser: true
-}, (error, client) => {
-if (error) return console.log('Unable to connect to database!')
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+  if (error) {
+    return console.log('Unable to connect to database!')
 
-const db = client.db(databaseName)
+  }
+  const db = client.db(databaseName)
 
-db.collection('users').insertOne({
-    name: 'Vero',
+  db.collection('users').insertOne({
+    Name: 'Vero',
     age: 32
-})
-}
-    )
+  }, (error, result) => {
+    if (error) {
+      return console.log('Unable to insert user')
+    }
+    console.log(result.ops)
 
+
+  })
+})
