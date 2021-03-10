833e07a9c0fffbb35a861ad09d752e26b6e134e4
veroca88 <veronicadonoso1506@gmail.com>
2021-03-09 21:53:59 -0600

    findOne element using new ObjectID()

--- mongodb.js
+++ mongodb.js
@@ -10,11 +10,16 @@ const databaseName = "todo-app"
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
   if (error) {
     return console.log('Unable to connect to database!')
-
   }
+
   const db = client.db(databaseName)
 
-  db.collection('task').findOne({},)
+  db.collection('task').findOne({ _id: new ObjectID("603d2673199fa05933443e74") }, (error, task) => {
+    if (error) {
+      return console.log('Unable to fetch!')
+    }
+    console.log(task)
+  })
 
 
 })
