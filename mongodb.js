3413bec67cb2eb365bfe1ae82ee2800930ec3e25
veroca88 <veronicadonoso1506@gmail.com>
2021-03-09 21:44:03 -0600

    Base form to start using any method

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
 
 const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const { MongoClient, ObjectID } = require("mongodb")
 
 const connectionURL = process.env.MONGODB_URI
 const databaseName = "todo-app"
+
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
   if (error) {
@@ -13,15 +14,7 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
   }
   const db = client.db(databaseName)
 
-  db.collection('users').insertOne({
-    Name: 'Vero',
-    age: 32
-  }, (error, result) => {
-    if (error) {
-      return console.log('Unable to insert user')
-    }
-    console.log(result.ops)
+  db.collection('task').findOne({},)
 
 
-  })
 })
